419576a05e19799097e8d5671dcb9529b96ef0b5
Dick van Viegen <dickv.viegen@gmail.com>
2017-02-24 00:07:54 +0100

    Changed About & Example pages title

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,8 +3,8 @@ var React = require('react');
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples Component</h3>
-            <p>Welcome to the examples page</p>
+            <h3>Examples</h3>
+            <p>Welcome to the examples page!</p>
         </div>
     )
 };
