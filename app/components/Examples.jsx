5b11774ad1f6d3a0c86287b337ad02439cd55548
Dick van Viegen <dickv.viegen@gmail.com>
2017-02-23 22:55:46 +0100

    update copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-        <h3>Examples Component</h3>
-        <p>Welcome to the examples page!</p>
+        <div>
+            <h3>Examples Component</h3>
+            <p>Welcome to the examples page!</p>
+        </div>
     )
 };
 
