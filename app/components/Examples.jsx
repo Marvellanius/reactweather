6a6e92ed9e98689c2a361a7746513da8effccf6a
Dick van Viegen <dickv.viegen@gmail.com>
2017-02-24 00:04:03 +0100

    another test commit

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,7 @@ var Examples = (props) => {
     return (
         <div>
             <h3>Examples Component</h3>
-            <p>Welcome to the examples page!</p>
+            <p>Welcome to the examples page</p>
         </div>
     )
 };
